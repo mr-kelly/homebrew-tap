d9c7ac95893d569f7aac4086535575b014a0d0aa
Kelly Chan <kellypeilinchan@gmail.com>
2026-02-22 14:07:54 +0800

    Add Mato formula for v0.4.0-alpha.2

---
+++ Formula/mato.rb
@@ -0,0 +1,33 @@
+class Mato < Formula
+  desc "Multi-Agent Terminal Office"
+  homepage "https://github.com/mr-kelly/mato"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mr-kelly/mato/releases/download/v0.4.0-alpha.2/mato-macos-aarch64-v0.4.0-alpha.2.tar.gz"
+      sha256 "21e0ae4f3f39b93dceae9fd0a8017b8d1a0da2782d19bbb94f0ce732d326abe5"
+    else
+      url "https://github.com/mr-kelly/mato/releases/download/v0.4.0-alpha.2/mato-macos-x86_64-v0.4.0-alpha.2.tar.gz"
+      sha256 "1c22984ad697ab591e157e8719ba33697891873c3b3db1787bd0b407890486d1"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/mr-kelly/mato/releases/download/v0.4.0-alpha.2/mato-linux-aarch64-v0.4.0-alpha.2.tar.gz"
+      sha256 "8a6be83782ec12701f4100ccf86303dd1ce4bb2af959c4898a90265db903bda0"
+    else
+      url "https://github.com/mr-kelly/mato/releases/download/v0.4.0-alpha.2/mato-linux-x86_64-v0.4.0-alpha.2.tar.gz"
+      sha256 "4a8270f6a04cfd7acaafd227bc44793373b1cf68acafdf298889835f4c9374e8"
+    end
+  end
+
+  def install
+    bin.install "mato"
+  end
+
+  test do
+    assert_match "mato", shell_output("#{bin}/mato --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Homebrew tap for Mato.
+
+## Install
+
+```bash
+brew tap mr-kelly/tap
+brew install mato
+```
+
+## Upgrade Formula
+
+Update `Formula/mato.rb` with new release URLs and SHA256 values.
